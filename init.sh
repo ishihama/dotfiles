8696366cc9e2549ac6f8d36a6115b9c223413eaf
REDACTED <REDACTED@example.com>
2019-12-04 19:49:57 +0900

    update

--- init.sh
+++ init.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-for i in $(find . ".*" -maxdepth 1); do
+for i in $(find . -name ".*" -not -name ".git" -not -name "." -maxdepth 1); do
   ln -s $(cd $(dirname $i) && pwd)/$i ~/$i
 done
 
