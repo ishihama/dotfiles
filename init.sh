3207b241f74ca484f9cf88c00b9b919ed89c8d18
ishihama <ishihama@lightcafe.co.jp>
2019-12-04 20:00:03 +0900

    update

--- init.sh
+++ init.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
+
+# ln -s dotfiles to $HOME
 for i in $(find . -name ".*" -not -name ".git" -not -name "." -maxdepth 1); do
   ln -s $(cd $(dirname $i) && pwd)/$i ~/$i
 done
+
+# oh-my-zsh
+curl -L https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh | sh
+
+# dein
+curl -L https://raw.githubusercontent.com/Shougo/dein.vim/master/bin/installer.sh | bash -s ~/.cache/dein
 
