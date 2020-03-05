bec7985e14c3a8dd772d80a0bdcbc00f363e7c43
ishihama <ishihama@lightcafe.co.jp>
2020-03-05 14:21:29 +0900

    Homebrewのインストールコマンドをアップデート

--- init.sh
+++ init.sh
@@ -9,7 +9,7 @@ done
 curl -L https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh | sh
 
 # brew
-/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
 
 brew bundle
 
